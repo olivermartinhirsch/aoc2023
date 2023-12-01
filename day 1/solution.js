ea69234e0a6176ff9185fb55c0f255e2175fa8c4
olivermartinhirsch <oliver.martinhirsch@gmail.com>
2023-12-01 09:49:26 +0000

    Day1 part1

---
+++ day 1/solution.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const parse = (source) => source.split("\n").filter(Boolean);
+
+const calibrationDocument = parse(fs.readFileSync("input.txt", "utf-8"));
+
+const partOne = (calibrationDocument) => {
+    const re = /\d/g
+    return calibrationDocument
+      .map((e) => e.match(re))
+      .map((e) => Number(e[0] + e[e.length - 1]))
+      .reduce((sum, value) => sum + value, 0)
+  };
+  
+  console.log(partOne(calibrationDocument));
