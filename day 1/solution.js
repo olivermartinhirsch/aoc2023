71a6284d532dc92c8495d62b1f593996d7c900d2
olivermartinhirsch <oliver.martinhirsch@gmail.com>
2023-12-01 12:03:12 +0000

    part2 solution

--- day 1/solution.js
+++ day 1/solution.js
@@ -13,3 +13,33 @@ const partOne = (calibrationDocument) => {
   };
   
   console.log(partOne(calibrationDocument));
+
+const partTwo = calibrationDocument => {
+
+    let total = 0;
+
+    const numberMappings = { 
+        one: "one1one", 
+        two: "two2two", 
+        three: "three3three", 
+        four: "four4four", 
+        five: "five5five", 
+        six: "six6six", 
+        seven: "seven7seven", 
+        eight: "eight8eight", 
+        nine: "nine9nine"
+    }
+
+    for (let line of calibrationDocument) { 
+        for (let num of Object.keys(numberMappings)) { 
+            line = line.replaceAll([num], numberMappings[num]); 
+        }
+
+        const numbers = line.split('').filter(el => !isNaN(parseInt(el)))
+        total += parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`); 
+        }
+
+    return total
+    }
+
+console.log(partTwo(calibrationDocument));
